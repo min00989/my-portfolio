4700b634d97710ffe2df0d62db0f2c48130b1d84
min00989 <dnflalswls@naver.com>
2024-11-12 16:18:44 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+import About from "./about/About";
+import "./App.css";
+import Skils from "./skils/Skils";
+import Contact from "./contact/Contact";
+import Projects from "./projects/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/skils" element={<Skils />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/contact" element={<Contact />} />
+          {/* 추가적인 Route 설정을 여기에 넣을 수 있습니다 */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
